20dd054ee70d9b1c7b2323fe070fde099911787b
Riley Mione <rmione33@gmail.com>
2020-06-26 16:50:52 -0400

    changed restart to shutdown, going to add perms to shutdown function so only i and maybe others can use it

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_GUILD')
 
 
-# client = discord.Client()
+
 creeper = commands.Bot(command_prefix='.')
 srv = observer.Server()
 
@@ -33,16 +33,16 @@ async def bing(ctx):
 @commands.cooldown(1, 60)
 async def speak(ctx, *, argument):
     """
-    On message it checks for it being mentioned, as well as if the keyword "where server" is mentioned.
-    This is the bot's main functionality as of yet.
-    Also there is  infinite loop protection!
-
-    :param message:
+    Speak handles all of the basic functionality of the bot.
+    It has a basic decision structure to let it respond to different messages.
+    It takes the context parameter, and then an additional argument.
     :return:
     """
 
     if ctx.message.author == creeper.user:
         return
+    if argument == ' ': 
+        await ctx.message.channel.send("Aw man")
 
     if re.match("<@?!"+SECRETS['id'], ctx.message.content):
         await ctx.message.channel.send("I am a bot created by @rmione on github, check me out: https://github.com/rmione/creeper")
@@ -66,7 +66,7 @@ async def speak(ctx, *, argument):
             # The server is down
             await ctx.message.channel.send("This server is down. Sorry for the inconvenience!")
 
-
+    
     if argument == 'help':
         await ctx.message.channel.send(
 
@@ -82,7 +82,11 @@ async def speak(ctx, *, argument):
         
         :boom:
         """)
-
-
+@creeper.command(name="shutdown")
+async def shutdown(ctx):
+    # Logs out and resets the internal state of the bot 
+    await creeper.close()
+    creeper.clear()
+    
 
 creeper.run(TOKEN)
