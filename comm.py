c98b111af299b291ca42881f8a6ea7fca7f77f2e
Riley Mione <rmione33@gmail.com>
2020-06-26 22:56:42 -0400

    sshing in works, it can turn off the server

--- comm.py
+++ comm.py
@@ -8,11 +8,15 @@ USER = os.getenv('SSH_USER')
 PASS = os.getenv('SSH_PASS')
 
 
+def server_shutdown():
+    client = paramiko.SSHClient() # instantiate class
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(SECRETS['ip'], username=USER, password=PASS) # Get IP from secrets file
 
-client = paramiko.SSHClient() # instantiate class
-client.load_system_host_keys()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(SECRETS['ip'], username=USER, password=PASS) # Get IP from secrets file
 
-client.exec_command('mkdir test12')
+    stdin_, stdout_, stderr_ = client.exec_command("screen -r -X eval 'stuff \"stop\"\\\\015'")
 
+    lines = stdout_.readlines()
+    for line in lines:
+        print(line)
