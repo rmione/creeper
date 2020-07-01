56d925c28b77de15d865800ad54f5cc712e5accc
Riley Mione <rmione33@gmail.com>
2020-07-01 15:39:51 -0400

    Creeper now dms you the readme basically

--- bot.py
+++ bot.py
@@ -12,43 +12,37 @@ from comm import WakeOnLAN
 from comm import server_shutdown
 import logging 
 
-PATH = (os.path.dirname(os.path.abspath(__file__)))
-logging.basicConfig(format='%(asctime)-15s s%(message)s', level='INFO', filename=PATH+'/log/creeper.log')
+# logging config 
+logging.basicConfig(format='%(asctime)-15s s %(message)s', level='INFO')
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 
 
-
+# This is all the dotenv loading...
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_GUILD')
 MACADDRESS = os.getenv('MACADDRESS')
 MAP = os.getenv('MAP') 
+POWERUSERS=os.getenv('POWERUSERS').split(',')
+PATH = (os.path.dirname(os.path.abspath(__file__)))
+
+
 
 creeper = commands.Bot(command_prefix='.')
 srv = observer.Server()
 
-
+# Actual code starts here.
 
 @creeper.event
 async def on_ready():
-    # logging.info(f"{creeper.user} is up and running!")
+    logging.info(f" {creeper.user} is up and running!")
     pass
 
 @creeper.command()
 async def bing(ctx):
     await ctx.send("Bong")
 
-@creeper.command(name="maintenance")
-@commands.cooldown(1, 60)
-async def maintenance(ctx, *, argument):
-    if argument ==  "start":
-        srv.maintenance = True
-        await ctx.message.channel.send("Maintenance mode started!")
-    if argument == "stop":
-        srv.maintenance = False
-        await ctx.message.channel.send("Maintenance mode ended!")
-    
 
 @creeper.command(aliases=['creeper'])
 @commands.cooldown(1, 60)
@@ -99,37 +93,73 @@ async def speak(ctx, *, argument):
         Now far ahead the Road has gone.
         Let others follow, if they can!
         Let them a journey new begin.
-        But I at last with weary feet
-        Will turn towards the lighted inn,
-        My evening-rest and sleep to meet.
             - J.R.R Tolkien
                 """
         await ctx.send("https://"+MAP+"\n"+ quote)
 
     if argument == 'help':
-        await ctx.message.channel.send(
+        # await ctx.message.channel.send(
 
-        """
-        :boom: 
+        # """
+        # :boom: 
         
-        creeper alpha 
-        A handy Minecraft-related discord bot
-        Commands
-        .creeper help
-        .creeper where server
+        # creeper alpha 
+        # A handy Minecraft-related discord bot
+        # Commands
+        # .creeper help
+        # .creeper where server
         
-        :boom:
-        """)
+        # :boom:
+        # """)
+        await ctx.message.author.send(open(PATH+"\\README.md").read())
 
     
 
 @creeper.command(name="wake")
 async def Wake(ctx):
-    await ctx.message.channel.send("Waking up server...")
+    await ctx.message.channel.send("Attempting to wake up server...")
     logging.info("{0} used the wake command. ".format(ctx.message.author))
-    logging.info("Waking up server...")
     waker = WakeOnLAN()
     packet = WakeOnLAN.magic_packet(waker, MACADDRESS)
     WakeOnLAN.send(waker, packet, 9)
     
+    # Now give it some time to wake up. There is no solid set time for waking it up. This will obvously be different case to case.
+    asyncio.sleep(3*60) # This works fine for my server!
+    try:
+        srv.info()
+        await ctx.message.send("Server is awake! Game on!")
+    except ConnectionRefusedError:
+        await ctx.message.channel.send("This address doesn't want me connecting to it.")
+    except socket.timeout:
+        # todo: move this all to a function that checks if it's online. Will be better for the code
+        await ctx.message.channel.send("the server is still down.")
+    
+def check_poweruser(ctx):
+    """
+    This command checks if the user is a poweruser (Defined in .env)
+    Used to allow access to some sensitive commands, etc...
+
+    """
+    
+    if str(ctx.message.author) in POWERUSERS:
+        logging.info("{0} passed poweruser check. ".format(str(ctx.message.author)))
+        return True
+    else:
+        logging.info("{0} failed poweruser check. ".format(str(ctx.message.author)))
+        return False 
+
+
+
+@creeper.command(name="maintenance")
+@commands.check(check_poweruser)
+@commands.cooldown(1, 60)
+async def maintenance(ctx, *, argument):
+    if argument ==  "start":
+        srv.maintenance = True
+        await ctx.message.channel.send("Maintenance mode started!")
+    if argument == "stop":
+        srv.maintenance = False
+        await ctx.message.channel.send("Maintenance mode ended!")
+    
+
 creeper.run(TOKEN)
